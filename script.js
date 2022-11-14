7f99d10f9050a55c0d932a5c1d5080d18a39271e
Hassaanjbaig-code <80938128+Hassaanjbaig-code@users.noreply.github.com>
2022-11-14 20:12:07 +0500

    Add hide function to scrip

--- script.js
+++ script.js
@@ -2,4 +2,8 @@ function showMobileMenu() {
     document.querySelector(".mobile-menu").style.display = "block";
     document.querySelector(".grid-container").style.display = "none";
   }
+function hideMobileMenu() {
+    document.querySelector(".mobile-menu").style.display = "none";
+    document.querySelector(".grid-container").style.display = "block";
+}
   
