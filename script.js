e56a95c76758b2ec77a5b1ec64f5ea5dcaa664f1
Mohammad (Billy) Mohajeri <mohajeri322@gmail.com>
2022-11-14 18:31:24 +0200

    Add style and content to the mobile menu

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+const ham = document.getElementById('ham');
+const x = document.getElementById('x');
+
 function showMobileMenu() {
-    document.querySelector(".mobile-menu").style.display = "block";
-    document.querySelector(".grid-container").style.display = "none";
-  }
-function hideMobileMenu() {
-    document.querySelector(".mobile-menu").style.display = "none";
-    document.querySelector(".grid-container").style.display = "block";
+  document.querySelector('.mobile-menu').style.display = 'block';
+  document.querySelector('.grid-container').style.display = 'none';
+  document.querySelector('.menu-footer').style.display = 'block';
+  document.querySelector('.toolbar').style.display = 'none';
 }
-  
+
+function hideMobileMenu() {
+  document.querySelector('.mobile-menu').style.display = 'none';
+  document.querySelector('.grid-container').style.display = 'grid';
+  document.querySelector('.menu-footer').style.display = 'none';
+  document.querySelector('.toolbar').style.display = 'flex';
+}
+
+ham.addEventListener('click', showMobileMenu());
+x.addEventListener('click', hideMobileMenu());
